5b552dfae9999db611c48c0a62f4a280ee49e693
valtervilmerson <valter.vilmerson@gmail.com>
2022-07-22 14:43:12 -0300

    new udate

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from db_connection import DbConnection
 from datetime import datetime
 from create_json import WriteJson
 
+trello_connection = TrelloConnection('ab47763a5af3b88111bbda128e1e5498')
+trello_connection.set_api_token('ffd5c4ea8ec16ae8c01258639c3dfe81f9f36adbb397ef2a5923a86a9a0c0a8b')
+trello_connection.set_board('62388d998a93181c0fe96d58')
+
 
 def main():
-    trello_connection = TrelloConnection('ab47763a5af3b88111bbda128e1e5498')
-    trello_connection.set_api_token('ffd5c4ea8ec16ae8c01258639c3dfe81f9f36adbb397ef2a5923a86a9a0c0a8b')
-    trello_connection.set_board('62388d998a93181c0fe96d58')
 
     db_connection = DbConnection('TRELLO_TECHSALLUS.db')
 
@@ -31,9 +32,28 @@ def main():
 
     db_connection.insert_cards_labels(trello_connection)
 
-    # actions = trello_connection.get_members_actions()
-    # print(actions)
-    # WriteJson.write("teste", actions)
+
+def update_cards():
+    list_id = '62388db39989c82d991ce470'
+    label_id = '62695479d0676034bae7fdf8'
+    new_list = '62388db5b91b032488cea097'
+
+    cards_certification = []
+    cards = trello_connection.get_trello_cards()
+    for card in cards:
+        if card['idList'] == list_id:
+            for label in card['idLabels']:
+                if label_id in label:
+                    cards_certification.append(card['id'])
+    print(cards_certification)
+    for card_id in cards_certification:
+        trello_connection.update_card_id_list(card_id, new_list)
+
+
+def create_json():
+    actions = trello_connection.get_members_actions()
+    print(actions)
+    WriteJson.write("teste", actions)
 
 
 if __name__ == '__main__':
--- trello_connection.py
+++ trello_connection.py
@@ -37,3 +37,11 @@ class TrelloConnection(TrelloApi):
     def get_members_actions(self):
         trello_members_actions = self.trello_connection.members.get_action('5d277e86ce62ae7ecc7a13c1')
         return trello_members_actions
+
+    def add_label_to_card(self, card_id: str):
+        add_label = self.trello_connection.cards.new_idLabel(card_id, '62c6cfabd2f01b44dbd0fe22')
+        return add_label
+
+    def update_card_id_list(self, card_id: str, id_list: str):
+        new_list = self.trello_connection.cards.update_idList(card_id, id_list)
+        return new_list
